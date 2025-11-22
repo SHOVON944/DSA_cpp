77dbe96e843cb7fc4bbaf37fe471fa12d63140cb
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-11-22 09:53:11 +0600

    add

---
+++ Efficiency type Algorithm code/Array length.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {0, 1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout<<size;
+
+    return 0;
+}
