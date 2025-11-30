cf692195b2f86d3d2f8afbf026f7af5dcccc74bb
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-11-30 23:37:55 +0600

    aadd

---
+++ Codee/College Wallah/pointer/basic.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a = 1;
+    // int *p = &a;
+    // int **pp = &p;
+    // cout<<a<<endl;
+    // cout<<&a<<endl;
+    // cout<<p<<endl;
+    // cout<<*p<<endl;
+    // cout<<**pp<<endl;
+
+    // float b = 3.5;
+    // float *q = &b;
+    // cout<<q<<endl;
+
+    // int *ptr;
+    // int x;
+    // cout<<x<<endl;
+    // cout<<ptr;
+
+
+    int *ptr;
+    int marks = 90;
+    ptr = &marks;
+    cout<<ptr<<endl;
+    cout<<*ptr<<endl;
+
+    *ptr = marks + 1;
+    cout<<*ptr<<endl;
+
+
+    return 0;
+}
