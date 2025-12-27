021196bdbdb80d5be22f523fa2050ce5e6795251
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-27 08:36:52 +0600

    add

---
+++ Codee/Apna College/Linked List/push_front.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{     // not access modifier declire. Means, it is private class
+    Node* head;
+    Node* tail;
+
+public:
+    List(){
+        head = tail = NULL;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val);      // dynamic
+        if(head == NULL){
+            head = tail = newNode;
+        } else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void printLL(){
+        Node* temp = head;
+        while(temp!=NULL){
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<" NULL"<<endl;
+    }
+
+};
+
+
+int main()
+{
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+
+    ll.printLL();
+
+    return 0;
+}
