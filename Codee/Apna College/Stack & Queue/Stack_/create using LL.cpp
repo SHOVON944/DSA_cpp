dcc7fc264c00fade757e807e64a152bf483a5c10
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-29 16:48:30 +0600

    add

--- Codee/Apna College/Stack & Queue/Stack_/create using LL.cpp
+++ Codee/Apna College/Stack & Queue/Stack_/create using LL.cpp
@@ -1,24 +1,26 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<list>
 using namespace std;
 
 class Stack{
-    vector<int> v;
+    list<int> ll;
 
 public:
     void push(int val){     // O(1)
-        v.push_back(val);
+        ll.push_front(val);
     }
 
     void pop(){     // O(1)
-        v.pop_back();
+        ll.pop_front();
     }
 
     int top(){     // O(1)
-        return v[v.size()-1];
+        return ll.front();
     }
 
     bool empty(){
-        return v.size()==0;
+        return ll.size()==0;
     }
 
 };
