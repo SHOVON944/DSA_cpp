c8d14b39c3da2eef0cc753c344b4d03397ed8e8c
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-31 10:34:37 +0600

    add

---
+++ Codee/Apna College/Tree/levelorder traversal.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+    // TC: O(n)
+static int idx = -1;
+Node* buildTree(vector<int> preOrder){
+    idx++;
+    if(preOrder[idx]==-1){
+        return NULL;
+    }
+
+    Node* root = new Node(preOrder[idx]);
+    root->left = buildTree(preOrder);   // left
+    root->right = buildTree(preOrder);  // right
+
+    return root;
+}
+
+    // TC: O(n)
+void levelOrderTraversal(Node* root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(q.size()>0){
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr==NULL){
+            if(!q.empty()){
+                cout<<endl;
+                q.push(NULL);
+                continue;
+            } else{
+                break;
+            }
+        }
+        cout<<curr->data<<" ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->left != NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+
+    Node* root = buildTree(preOrder);
+    levelOrderTraversal(root);
+
+    return 0;
+}
