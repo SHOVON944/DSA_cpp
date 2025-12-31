a6b241e211ea9f366bfb9b078454d506a8eafbc1
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2025-12-31 16:31:29 +0600

    add

---
+++ Codee/Apna College/Tree/Diameter of binary tree.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=543 lang=cpp
+ *
+ * [543] Diameter of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+
+        int leftHt = height(root->left);
+        int rightHt = height(root->right);
+
+        return max(leftHt, rightHt) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+
+        int leftDiam = diameterOfBinaryTree(root->left);
+        int rightDiam = diameterOfBinaryTree(root->right);
+
+        int rootPassDiam = height(root->left) + height(root->right);
+        return max(rootPassDiam, max(leftDiam, rightDiam));
+    }
+};
+// @lc code=end
