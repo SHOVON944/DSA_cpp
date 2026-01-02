aa3b3f832005a3e2501eff4c7021d49a825b9a47
SHOVON944 <mdsakibulislamshovon944@gmail.com>
2026-01-02 16:28:03 +0600

    add

---
+++ Codee/Apna College/Tree/Binare Search Tree (BST)/Sorted array to balanced BSTs.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=108 lang=cpp
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(vector<int> &nums, int st, int end){
+        if(st>end){
+            return NULL;
+        }
+
+        int mid = st + (end-st)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = helper(nums, st, mid-1);
+        root->right = helper(nums, mid+1, end);
+
+        return root;
+    }
+
+
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size()-1);
+    }
+};
+// @lc code=end
